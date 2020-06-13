ded6cdbaf15aebb1b929a344b4c3c22051e4c002
wjase <wjase@yahoo.com.au>
2020-06-13 13:22:00 +1000

    Added bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt install snapd
+sudo apt install git
+sudo apt update
+sudo apt install software-properties-common
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible
